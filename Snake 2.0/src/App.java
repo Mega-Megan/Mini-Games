3757c4520d952ce3c1a1d6db989d6a03db6f3b95
Megan Boyle <119425357+Mega-Megan@users.noreply.github.com>
2023-12-27 17:54:49 -0500

    Add files via upload

---
+++ Snake 2.0/src/App.java
@@ -0,0 +1,26 @@
+
+//Imports
+import javax.swing.*;
+
+public class App {
+    public static final String frame = null;
+    public static Object restartButton;
+
+    public static void main(String[] args) throws Exception {
+        int boardWidth = 500;
+        int boardHeight = boardWidth;
+        JFrame frame = new JFrame("Snake");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        SnakeGame snakeGame = new SnakeGame(boardWidth, boardHeight);
+        frame.add(snakeGame);
+        frame.pack();
+        snakeGame.requestFocus();
+
+    }
+
+}
